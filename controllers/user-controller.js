090343d885591227343c229f4c3979fc4e37ad27
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-07-16 15:19:53 -0400

    finished user controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -76,6 +76,36 @@ const userController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: params.friendId },
+      { new: true }
+    ).then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "no user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    });
+  },
 };
 
 module.exports = userController;
